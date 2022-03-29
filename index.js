3788db940b4cac1cd68b3e29be82c29f28bb15c3
Karsten <karstenhertenstein@mail.de>
2022-03-29 17:07:43 +0200

    blub

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import usersRoutes from './routes/users.js';
 const app = express();
 const PORT = 5000;
 
-const URL = 'mongodb+srv://js_mastery:js_mastery@practice.jto9p.mongodb.net/users?retryWrites=true&w=majority'
+const URL = 'mongodb+srv://root:root@cluster0.obtnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
